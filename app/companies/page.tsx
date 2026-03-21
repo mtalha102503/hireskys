e163358e05d19ece3e49a2321da98a1b5891633a
mtalha102503 <realonlinejobs56@gmail.com>
2026-03-22 00:47:25 +0500

    Update page.tsx

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -114,7 +114,7 @@ export default function CompaniesPage() {
             if (nums) {
               const parsedNums = nums.map((n: string) => parseInt(n.replace(/,/g, ''), 10)).filter((n: number) => n > 1000); // Saal/Mahina ignore karne ke liye >1000
               if (parsedNums.length > 0) {
-                const avg = parsedNums.reduce((a, b) => a + b, 0) / parsedNums.length;
+                const avg = parsedNums.reduce((a: number, b: number) => a + b, 0) / parsedNums.length;
                 totalSalary += avg;
                 validSalaries++;
               }

987604b76f167b353e264ef36fa50f53c75f387a
mtalha102503 <realonlinejobs56@gmail.com>
2026-05-17 20:24:53 +0500

    Update route.ts

--- app/api/webhook/fastspring/route.ts
+++ app/api/webhook/fastspring/route.ts
@@ -5,7 +5,8 @@ import { createClient } from '@supabase/supabase-js';
 // 🚨 Webhooks backend par chalte hain isliye inhein Admin level access chahiye hota hai
 const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! 
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! 
+);
 
 export async function POST(req: Request) {
   try {
